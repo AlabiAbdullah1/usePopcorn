93beaabbb69ab9099d0effec4da0d63b6ce878f5
Alabiabdullahi <abdullahialabi234@gmail.com>
2024-08-10 16:05:47 +0100

    usePopcorn

---
+++ @latest/src/main.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import App from './App.js'
+import "./index.css";
+// import AppV1 from "./App v1.jsx";
+import AppV2 from "./App v2.jsx";
+// import StarRating from "./StarRating";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <AppV1 /> */}
+    <AppV2 />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+      defaultRating={3}
+    /> */}
+    {/* <Test/> */}
+  </React.StrictMode>
+);
---
+++ @latest/src/useMovie.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+// import.meta.env.VITE_
+
+// const KEY = process.
+
+export function useMovie(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      // Check if any call back function is called
+      //   callback?.();
+
+      const controller = new AbortController();
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          // Handle the error if there is no internet:
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching the movies");
+
+          //   if (!res.ok) {
+          //     return setError("Failed to fetch Movie, Check Internet");
+          //   }
+
+          const data = await res.json();
+
+          if (data.Response === false) {
+            throw new Error("Movie not found!");
+          }
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          console.error(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      //   handleCloseMovie();
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
